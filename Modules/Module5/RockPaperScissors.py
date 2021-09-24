45b26faed72ac7742c52c6e7f02628fde7ec3c98
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2021-09-23 20:24:23 -0700

    Create RockPaperScissors.py
    Me puedes dar hasta manana porfavor. Es que estoy perdido.

---
+++ Modules/Module5/RockPaperScissors.py
@@ -0,0 +1,19 @@
+import random
+
+Osvaldochoice=input("rock","paper","scissors")
+Computerchoice:random.choice("scissors","rock","paper")
+print(f"Computer selected: {Computerchoice}")
+x = 45
+
+if(x == 0):
+    print("The value of x is 0")
+elif(x > 50):
+  print("The value of x is more than 50")
+else:
+    print("The value of x is lower than 50, and is not 0")
+
+    import random
+
+foo = ['rock', 'paper', 'scissors']
+computerChoice = random.choice(foo)
+print(f"Computer selected: {computerChoice}")
