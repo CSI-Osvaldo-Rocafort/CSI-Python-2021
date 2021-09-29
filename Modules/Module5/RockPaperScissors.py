8fbe5ade25b8584f0df97baa368742fed2b0519b
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2021-09-29 07:30:52 -0700

    Update RockPaperScissors.py

--- Modules/Module5/RockPaperScissors.py
+++ Modules/Module5/RockPaperScissors.py
@@ -1,30 +1,31 @@
 import random
 
-Osvaldochoice=input("rock","paper","scissors")
-Computerchoice:random.choice("scissors","rock","paper")
+mychoice = input("rock, paper, or scissors")
+print(mychoice)
+
+computerChoice = ("rock","paper","scissors")
+Computerchoice = random.choice(computerChoice)
 print(f"Computer selected: {Computerchoice}")
-x = 45
 
-if(x == 0):
-    print("The value of x is 0")
-elif(x > 50):
-  print("The value of x is more than 50")
-else:
-    print("The value of x is lower than 50, and is not 0")
+# Based on what the computer chose, who wins 
 
-    import random
-
-foo = ['rock', 'paper', 'scissors']
-computerChoice = random.choice(foo)
-print(f"Computer selected: {computerChoice}")
-
-if(computerChoice == Osvaldochoice):
+if(computerChoice == "rock" and mychoice == "rock"):
      print("tied")
-elif(computerChoice == "scissors" and Osvaldochoice == "rock"):
-     print("Iwin")
-elif(computerChoice == "paper" and Osvaldochoice == "rock"):
+elif(computerChoice == "scissors" and mychoice == "rock"):
+     print("You win")
+elif(computerChoice == "paper" and mychoice == "rock"):
      print("computerwon")
-elif(computerChoice == "rock" and Osvaldochoice == "paper"):
-    print("Iwin")
-elif(computerChoice == "paper" and Osvaldochoice == "scissors"):
-    print("Iwin")
+elif(computerChoice == "paper" and mychoice == "paper"):
+    print("tied")
+elif(computerChoice == "rock" and mychoice == "paper"):
+    print("You win")
+elif(computerChoice == "paper" and mychoice == "scissors"):
+    print("You Win")
+elif(computerChoice == "scissors" and mychoice == "scissors"):
+    print("tied")
+elif(computerChoice == "scissors" and mychoice == "paper"): 
+    print("computerwon")
+elif(computerChoice == "rock" and mychoice == "paper"):
+    print("You win")
+elif(computerChoice == "scissors" and mychoice == "rock"):
+    print("You win")
