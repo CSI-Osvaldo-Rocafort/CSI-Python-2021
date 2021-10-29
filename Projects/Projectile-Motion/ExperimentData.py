2f285076df8e88a94b40b7e379e7ea6faf59abc4
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2021-10-29 06:17:16 -0700

    Update ExperimentData.py

---
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -0,0 +1,18 @@
+from _typeshed import Self
+
+
+class ExperimentData: 
+    def __init_(Self, Gunname:str, Cartrigecalibre:str, Round:str, Roundvelocity:str, Building:str, Buildingheight:str, gravity:str )
+        Self.Gunname = Gunname
+        Self.Cartridgecalibre = Cartridgecalibre
+        Self.Round = Round 
+        Self.Roundvelocity = Roundvelocity
+        Self.Building = Building
+        Self.Buildingheight = Buildingheight
+        Self.gravity = gravity
+
+
+
+
+
+
