6c417a7498c040791b00454ba93dcb3347a9796d
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2021-10-29 07:39:52 -0700

    Update ExperimentData.py

--- Projects/Projectile-Motion/ExperimentData.py
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -1,16 +1,32 @@
-from _typeshed import Self
-
-
+import math 
 class ExperimentData: 
-    def __init_(Self, Gunname:str, Cartrigecalibre:str, Round:str, Roundvelocity:str, Building:str, Buildingheight:str, gravity:str )
-        Self.Gunname = Gunname
-        Self.Cartridgecalibre = Cartridgecalibre
-        Self.Round = Round 
-        Self.Roundvelocity = Roundvelocity
-        Self.Building = Building
-        Self.Buildingheight = Buildingheight
-        Self.gravity = gravity
+    def __init_(self, Gunname:str, Cartrigecalibre:str, Round:str, Roundvelocity:int, Building:str, Buildingheight:int, gravity:int ):
+        self.Gunname = Gunname
+        self.Cartridgecalibre = Cartrigecalibre
+        self.Round = Round 
+        self.Roundvelocity = Roundvelocity
+        self.Building = Building
+        self.Buildingheight = Buildingheight
+        self.gravity = gravity
 
+    def getTime(self):
+        return (math.sqrt(2 * self.Buildingheight / self.getGravity()))
+
+    def getDistance(self):
+        return (self.Roundvelocity * self.getTime)
+        
+    def getGravity(self):
+        planets = ["Earth", "Mercury", "Venus", "Jupiter", "Mars", "Uranus", "Saturn"]
+        g_ms2 = [9.81, 3.7, 8.87, 24.79, 10.44, 11.15, 8.69]
+        return g_ms2[planets.index(self.planet)]
+    
+    def run(self):
+        print (f"I shot a {Gunname} from {Building} and the {Buildingheight} meters high. It goes at {Roundvelocity} and it takes about{time} seconds to reach {DeltaX}.")
+
+
+
+        
+ 
 
 
 
