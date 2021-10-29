2e65723f42b02cbbc92081d905d2a559f3785a23
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2021-10-29 06:17:12 -0700

    Update Osvaldo-Rocafort-Projectile.py

--- Projects/Projectile-Motion/Osvaldo-Rocafort-Projectile.py
+++ Projects/Projectile-Motion/Osvaldo-Rocafort-Projectile.py
@@ -1,4 +1,7 @@
 import math
+import os
+from ExperimentData import ExperimentData
+import json
 
 Gunname = ("DVL-10")
 Cartridgecalibre = ("7.62x51mm NATO")
@@ -13,5 +16,46 @@ gravity = 9.8
 
 # I searched for the tallest building in the world and chose the Burj Khalifa
 
-time = (math.sqrt(2 * Buildingheight/gravity))
-DeltaX = (Roundvelocity/time)
+time = (math.sqrt(2 * ExperimentData.Buildingheight/ExperimentData.gravity))
+DeltaX = (ExperimentData.Roundvelocity/time)
+
+print(f"I shot a {Gunname} from {Building} and the {Buildingheight} meters high. It goes at {Roundvelocity} and it takes about{time} seconds to reach {DeltaX}.")
+
+mydata = ExperimentData
+
+# expirementData= { 
+#     "Gunname" : "DVL-10",
+# "Cartridgecalibre" : "7.62x51mm NATO",
+# "Round" :  "7.62x51mm M61",
+# "Roundvelocity" : 849,
+# "Building" : "Burj Khalifa",
+# "Buildingheight" : 555
+# ,
+# "gravity" : 9.8
+# }
+
+# n"Gunname" : "DVL-10",
+# "Cartridgecalibre" : "7.62x51mm NATO",
+# "Round" :  "7.62x51mm M61",
+# "Roundvelocity" : 849,
+# "Building" : "Burj Khalifa",
+# "Buildingheight" : 555,
+# "gravity" : 9.8
+# }
+print(ExperimentData["Gunname", "7.62k51mm NATO", "7.62x51mm M61","Burj Khalifa","555", "9.8"])
+experiment:(mydata)
+#Sereliazation
+myOutpuPath = os.path(__file__).parent[0]
+myOutputPath = os.path.join(myOutpuPath, 'experimentData.json')
+
+with open(myOutpuPath,'w') as outfile:
+    json.dump(mydata._dict_,outfile)
+
+#Deserialization
+deserialize = open(myOutpuPath,)
+experimentJson = json.lead(deserialize)
+
+for o in experimentJson:
+    experiment(**e)
+    
+    # ExperimentData(##e).run()
