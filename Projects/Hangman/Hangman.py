54fe3530396cc8b194e5b471a9f9bfa94b0269ae
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2022-03-01 10:57:10 -0400

    Update Hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -16,11 +16,12 @@ def get_color():
 
     color:Colors = Colors(**requestData)
     
-    return (color.hex_value)
+    return (color.color_name)
 
 myColor = get_color()
+print(myColor)
 
-
+#Steps to create a hangman.
 steps = ["""
     .-------.
     |       |
@@ -75,34 +76,57 @@ steps = ["""
 #     print(steps)
 # print(steps[0])
 
+# Naming all the invalid characters in the keyboard for hangman.
 def getInput():
     Invalid_Characters= ("1","2","3","4","5","6","7","8","9","0","10","!","@","#","$","%","^","^","&","*","(",")","-","_","=","+","[","]","{","}",":",";",",","<",".",">","/","?")
-
+#Inputing a letter to choose.
     while(True):
         letter = input("Choose letter").upper()
-        
+# Putting the length of the letter
         if(len(letter)!=1):
             print("error")
             continue
-        
+# If you put a character that is from the invalid list it puts error.   
         if(letter in Invalid_Characters):
             print("error")
             continue
-        
+# Adding the letters to the list     
+        used.append(letter)
+# Returning a letter       
         return letter
     
-print(getInput())
+# It's defining print word and using temp
 
 def printword():
     temp:str=""
-    
+# If the letter that I choose is a letter from the word then it adds the letter to form the word and if I put a letter that is not from the word there will be a space.
     for letter in myColor:
-        
         if letter in used:
             temp=temp + letter
-            
+        else:
+            temp= temp + "_"
+ #printing temp           
+    print(temp)
+    
+    
+#printing steps for the counter of the words that are printing      
+def printSteps():
+    counter= 0
+    for letter in used:
+        if letter not in myColor:
+            counter= counter + 1
+#printing steps counter           
+    print(steps[counter])
+    
+# printing the steos, input and words for the hangman game.       
+while True:
+    printSteps()
+    getInput()
+    printword()
+    
+    
         
-            print()
+        
         
     
     
