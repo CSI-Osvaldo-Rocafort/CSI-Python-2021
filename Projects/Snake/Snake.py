978700d04a0fb64ed007199f14bee0b2321625a9
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2022-03-31 10:01:15 -0400

    Create Snake.py
    Step 1

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,7 @@
+import pygame  #importing python game
+pygame.init()  #initialize all imported pygame modules
+dis=pygame.display.set_mode((400,300)) #choosing the size of the display
+pygame.display.update() #to make the display Surface actually appear on the user's monitor.
+pygame.quit() #it runs code that deactivates the Pygame library
+quit() #is used to exit a python program. 
+
