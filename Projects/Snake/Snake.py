6f95ff7c0963fd23b0766d2a9dd2c6ef9bab184f
CSI-Osvaldo-Rocafort <24-102@sanignacio.pr>
2022-03-31 11:12:22 -0400

    Update Snake.py
    Step 3

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -6,7 +6,9 @@ pygame.display.set_caption('Snake game by Edureka') #Naming the game
 game_over=False #game is not over
 while not game_over: #while game is not over it keeps going
     for event in pygame.event.get():#registering all events from the user into an event
-        print(event)   #printing event
+        if event.type==pygame.QUIT: #Pygame quits when pygame equal game over
+            game_over=True #Game is over
+        print(event)  #printing event
 pygame.quit() #runinng the code that deactivates the Pygame library
-quit() #it's used to exit a python program. 
+quit() #it's used to exit a python program
 
